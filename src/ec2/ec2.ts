177330ac09a1f605998de33e5cb3ac8264e3ff1b
Diego Cárcamo <diego.carcamo@axity.com>
2024-07-15 09:23:11 -0600

    remove logs

--- src/ec2/ec2.ts
+++ src/ec2/ec2.ts
@@ -418,7 +418,6 @@ export class Ec2Instance {
         Values: [tag.Value],
       });
     }
-    core.info(`FILTERS: ${JSON.stringify(filters)}`);
     try {
       var params = {
         Filters: filters,
